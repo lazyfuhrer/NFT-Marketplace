43376e4eacd7e57036a34bc6e302dd2542f2bf46
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-08 14:37:55 +0530

    added rainbow & wagmi

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Navbar() {
     return (
--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,9 +1,41 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { polygon, optimism, arbitrum, goerli, polygonMumbai } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [polygonMumbai, goerli, polygon, optimism, arbitrum],
+  [
+    alchemyProvider({ apiKey: "DhZofW7i31Pskbr7kzmQEtNXiU28sbJZ" }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'NFT Marketplace',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider coolMode chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </ChakraProvider>
   ) 
 }
