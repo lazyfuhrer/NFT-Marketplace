c7ac1abe1416898b82241f81dcb8d88f1c86ec8e
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-10 15:50:25 +0530

    fxed listing nft functionality

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -16,13 +16,13 @@ import {
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../../pinata";
 import Marketplaceabi from "../../abi/Marketplaceabi.json"
 
-export default function Sell() {
+export default function Upload() {
 
-    //const marketplaceContract = useMarketplaceContract();
     const [formParams, updateFormParams] = useState({ name: '', description: '', price: ''});
     const [fileURL, setFileURL] = useState(null);
     const ethers = require("ethers");
     const [message, updateMessage] = useState('');
+    //const location = useLocation();
 
     //This function uploads the NFT image to IPFS
     async function OnChangeFile(e) {
@@ -70,72 +70,68 @@ export default function Sell() {
 
         //Upload data to IPFS
         try {
-          const metadataURL = await uploadMetadataToIPFS();
-          //After adding your Hardhat network to your metamask, this code will get providers and signers
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const signer = provider.getSigner();
-          updateMessage("Please wait.. uploading (upto 5 mins)")
+            const metadataURL = await uploadMetadataToIPFS();
+            //After adding your Hardhat network to your metamask, this code will get providers and signers
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            updateMessage("Please wait.. uploading (upto 5 mins)")
 
-          //Pull the deployed contract instance
-          let contract = new ethers.Contract("0xc49e0E80aF12a0937881461609b6EFFe3fFb977B", Marketplaceabi, signer)
-          console.log(contract)
+            //Pull the deployed contract instance
+            let contract = new ethers.Contract("0xc49e0E80aF12a0937881461609b6EFFe3fFb977B", Marketplaceabi, signer)
 
-          //massage the params to be sent to the create NFT request
-          const price = ethers.utils.parseUnits(formParams.price, 'ether')
-          let listingPrice = await contract.getListPrice()
-          listingPrice = listingPrice.toString()
+            //massage the params to be sent to the create NFT request
+            const price = ethers.utils.parseUnits(formParams.price, 'ether')
+            let listingPrice = await contract.getListPrice()
+            listingPrice = listingPrice.toString()
 
-          //actually create the NFT
-          let transaction = await contract.createToken(metadataURL, price, { value: listingPrice })
-          await transaction.wait()
+            //actually create the NFT
+            let transaction = await contract.createToken(metadataURL, price, { value: listingPrice })
+            await transaction.wait()
 
-          alert("Successfully listed your NFT!");
-          updateMessage("");
-          updateFormParams({ name: '', description: '', price: ''});
-          window.location.replace("/")
-      }
+            alert("Successfully listed your NFT!");
+            updateMessage("");
+            updateFormParams({ name: '', description: '', price: ''});
+            window.location.replace("/Marketplace")
+        }
         catch(e) {
             alert( "Upload error"+e )
         }
     }
 
-    //console.log(marketplaceContract)
+    console.log("Working", process.env);
+
 
   return (
     <>
-      <Box mt={'10'} alignItems="center" display="flex">
-        <Box
-          borderWidth="1px"
-          rounded="lg"
-          shadow="1px 1px 3px rgba(0,0,0,0.3)"
-          maxWidth={800}
-          p={6}
-          m="10px auto"
-          as="form">
-          <Progress
-            hasStripe
-            value={'50'}
-            mb="5%"
-            mx="5%"
-            isAnimated></Progress>
+    <Box mt={'10'} alignItems="center" display="flex">
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        maxWidth={800}
+        p={6}
+        m="10px auto"
+        as="form">
+        <Progress
+          hasStripe
+          value={'50'}
+          mb="5%"
+          mx="5%"
+          isAnimated>
+        </Progress>
 
-        <Heading fontSize={'20px'} w="100%" textAlign={'center'} fontWeight="normal" mb="13%">
-          Upload your NFT to the marketplace
-        </Heading>
-      
-        <FormControl mr="5%">
-          
-          <FormLabel htmlFor="name" fontWeight={'normal'}>
-              NFT Name
-          </FormLabel>
-          <Input id="name" placeholder="Axie#4563" value={formParams.name}
-                    onChange={(e) =>
-                      updateFormParams({
-                        ...formParams,
-                        name: e.target.value,
-                      })
-          }/>
-          
+      <Heading fontSize={'20px'} w="100%" textAlign={'center'} fontWeight="normal" mb="13%">
+        Upload your NFT to the marketplace
+      </Heading>
+
+      <FormControl mr="5%">
+        <FormLabel htmlFor="name" fontWeight={'normal'}>
+            NFT Name
+        </FormLabel>
+        <Input id="name" placeholder="Axie#4563" onChange={e => updateFormParams({...formParams, name: e.target.value})} value={formParams.name} />
+      </FormControl>
+
+      <FormControl mt={1}>
           <FormLabel
             htmlFor="description"
             mt={'5'}
@@ -145,29 +141,25 @@ export default function Sell() {
             _dark={{
               color: 'gray.50',
             }}>
-                NFT Description
+            NFT Description
           </FormLabel>
           <Textarea
             id="description"
-            placeholder="Axie Infinity Collection"
+            placeholder="you@example.com"
             rows={3}
             shadow="sm"
             focusBorderColor="brand.400"
             fontSize={{
               sm: 'sm',
             }}
-            value={formParams.description}
-                  onChange={(e) =>
-                    updateFormParams({
-                      ...formParams,
-                      description: e.target.value,
-                    })
-                  }
+            value={formParams.description} onChange={e => updateFormParams({...formParams, description: e.target.value})}
           />
           <FormHelperText>
             Brief description for your NFT.
           </FormHelperText>
-        
+        </FormControl>
+
+        <FormControl mt={'1'}>
           <FormLabel
             htmlFor="price"
             mt={'5'}
@@ -191,34 +183,35 @@ export default function Sell() {
             </InputLeftAddon>
             <Input
               type="number"
-              step="0.01"
+              step={'0.01'}
               placeholder="min 0.01"
               focusBorderColor="brand.400"
               rounded="md"
-              value={formParams.price}
-                  onChange={(e) =>
-                    updateFormParams({
-                      ...formParams,
-                      price: e.target.value,
-                    })
-                  }
+              value={formParams.price} onChange={e => updateFormParams({...formParams, price: e.target.value})}
             />
           </InputGroup>
-        
-          <FormLabel htmlFor="image" mt={'5'} fontWeight={'normal'}>
+        </FormControl>
+      
+      <FormControl mt="2%">
+        <FormLabel htmlFor="image" mt={'5'} fontWeight={'normal'}>
             Upload Image
-          </FormLabel>
-          <Input type={'file'} onChange={OnChangeFile} />
+        </FormLabel>
+        <Input type="file" onChange={OnChangeFile} />
+      </FormControl>
 
-          </FormControl>
+      <Box>{message}</Box>
 
-          <Flex mt="6" w="100%" justifyContent="space-between">
-            <Flex>
-                <Button w="7rem" colorScheme="red" variant="solid" onClick={listNFT}>List NFT</Button>
-            </Flex>
-          </Flex>
+      <Flex w="100%" mt="6" justifyContent="space-between">
+        <Button
+          w="7rem"
+          colorScheme="red"
+          variant="solid"
+          onClick={listNFT}>
+          List NFT
+        </Button>
+      </Flex>
       </Box>
-      </Box>
+      </Box>  
     </>
   );
-}
+};
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,4 @@
 import Landing from "@/components/Landing";
-
 export default function Home() {
 
   return (
